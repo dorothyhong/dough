7759529c534b682bdd560f6aba53e8ac94488e50
Nayan Paavalar <nayanpaa@umich.edu>
2024-02-18 01:51:02 -0500

    fixed eslint errors in comments.jsx

--- insta485/js/comments.jsx
+++ insta485/js/comments.jsx
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-//condition ? exprIfTrue : exprIfFalse
+// condition ? exprIfTrue : exprIfFalse
 
 export default function Comments({ comments, handleDeleteComment }) {
 
     
-    const renderedCommentList = comments.map((comment, i) => {
-        return <div key={i} >
-            <a href={`/users/${ comment.owner }/`}>{comment.owner}</a>
-            <div>{comment.text}</div>
-            <div>{comment.lognameOwnsThis 
-            ? <button onClick={handleDeleteComment(comment.commentid)}>Delete</button> : " " }</div>
-
-        </div>
-    })
+    const renderedCommentList = comments.map((comment) => (
+        
+            <div key={comment.commentid} >
+                <a href={`/users/${ comment.owner }/`}>{comment.owner}</a>
+                <div>{comment.text}</div>
+                <div>{comment.lognameOwnsThis 
+                ? <button type="button" onClick={() => handleDeleteComment(comment.commentid)}>Delete</button> : " " }
+                </div>
+            </div>
+            
+    ));
+    
 
     return (
         <div>{renderedCommentList}</div>
@@ -31,7 +34,8 @@ Comments.propTypes = {
         ownerShowUrl: PropTypes.string.isRequired,
         text: PropTypes.string.isRequired,
         url: PropTypes.string.isRequired,
-      })
+      }),
     ).isRequired,
+    handleDeleteComment: PropTypes.func.isRequired,
 };
   
